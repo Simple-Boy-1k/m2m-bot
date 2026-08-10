bc068da9a1ddff097e59642566c2c0d1e01bf6ea
𝘚𝘪𝘮𝘱𝘭𝘦_𝘉𝘰𝘺 <sksahnawaj89@gmail.com>
2026-08-10 23:32:10 +0530

    Update BOT_TOKEN in main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from pyrogram.errors import UserAlreadyParticipant
 # ==================== CONFIGURATION ====================
 API_ID = 31551910
 API_HASH = "c2e8e7946d5e4ea947d44b674008f33e"
-BOT_TOKEN = "8595762999:AAHmNthQFpGot6_MWtW00lB7xMRztmYHz1I"
+BOT_TOKEN = "8595762999:AAHNgNIHeWZLvcp5zr_6zJ3TTxe7u3aXpa8"
 
 SESSIONS_FILE = "sessions.json"
 
