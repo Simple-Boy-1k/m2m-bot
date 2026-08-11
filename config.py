147e7db739441078326ace2afafecccc8c9ff4eb
𝘚𝘪𝘮𝘱𝘭𝘦_𝘉𝘰𝘺 <sksahnawaj89@gmail.com>
2026-08-12 02:32:18 +0530

    Remove dynamic button color configuration
    Removed dynamic button color configuration from settings.

--- config.py
+++ config.py
@@ -6,5 +6,3 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "")
 OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
 
-# Dynamic Button Colour Config
-BUTTON_COLOUR = os.environ.get("BUTTON_COLOUR", "True").lower() in ("true", "1", "t")
